3b9b3d4f77008650321000200de5ac517bad618a
C Natalí Palacio Pastor <cristinanatalip@gmail.com>
2019-10-18 20:16:25 -0300

    Added animation when you click it, and remove that animation after it's duration. Re-structured the initial code, separating it in diferent functions when possible

--- script.js
+++ script.js
@@ -1,8 +1,44 @@
-window.addEventListener( "keydown", function (e){
+/* window.addEventListener( "keydown", function (e){
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    console.log(audio)
+    
 
     if (!audio) return; //stop function if the user pushes a button whitout a sound
     audio.currentTime = 0;
     audio.play();
+
+    //animation
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+    key.classList.add("playing");
+    
 });
+
+function removeTransition(e){
+    if (e.propertyName === "transform") return; //no pasa si no es un transform
+    e.target.classList.remove("playing");
+};
+
+const keys = document.querySelectorAll(".key");
+
+keys.forEach( key => key.addEventListener("transitionend", removeTransition)); */
+function playBardo(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+    if(!audio) return;
+
+    key.classList.add("playing");
+    audio.currentTime = 0;
+    audio.play();
+
+};
+
+function removeTransition(e){
+    if (e.propertyName === "transform") return; //no pasa si no es un transform
+    e.target.classList.remove("playing");
+};
+
+const keys = document.querySelectorAll(".key");
+
+keys.forEach( key => key.addEventListener("transitionend", removeTransition));
+window.addEventListener("keydown", playBardo);
